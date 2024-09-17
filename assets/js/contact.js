cec30e15cf663fcb2a0aa15d36cb591c41a887d5
gaudet <andregaudet225@gmail.com>
2024-09-17 11:17:46 +0000

    delete contact

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -59,6 +59,7 @@ var form = document.getElementById("my-form");
 async function handleSubmit(event) {
 event.preventDefault();
 var status = document.getElementById("my-form-status");
+
 var data = new FormData(event.target);
 fetch(event.target.action, {
   method: form.method,
